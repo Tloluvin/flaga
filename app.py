39e34c02173a0d4f375ed64fca84921b3939766f
Tloluvin <tloluvin.cbc@gmail.com>
2022-03-14 20:40:22 +0000

    wydzielenie hasła

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from moje_programy.character_wiki import character
 from moje_programy.open_poem import open_poem
+from moje_programy.haslo import haslo
 import os
 import random
 import string
@@ -22,40 +23,8 @@ def xd():
 
 @app.route('/haslo')
 def haslo():
-    lowercase = string.ascii_lowercase
-    uppercase = string.ascii_uppercase
-    digits = string.digits
-    punctuation = string.punctuation
-    wszystkie_znaki = lowercase + uppercase + digits + punctuation
-    dlugosc_hasla = 10
-    min_malych_liter = 2
-    min_duzych_liter = 2
-    min_cyfr = 2
-    min_znakow_specjalnych = 2
-    wynik = ''
-    if ((min_cyfr + min_duzych_liter + min_malych_liter + min_znakow_specjalnych) <= dlugosc_hasla):
-        if min_malych_liter > 0:
-            for c in range(min_malych_liter):
-                wynik += random.sample(lowercase, 1)[0]
-        if min_duzych_liter > 0:
-            for c in range(min_duzych_liter):
-                wynik += random.sample(uppercase, 1)[0]
-        if  min_cyfr > 0:
-            for c in range(min_cyfr):
-                wynik += random.sample(digits, 1)[0]
-        if min_znakow_specjalnych > 0:
-            for c in range(min_znakow_specjalnych):
-                wynik += random.sample(punctuation, 1)[0]
-
-        if len(wynik) < (dlugosc_hasla):
-            for c in range(dlugosc_hasla - len(wynik)):
-                wynik += random.sample(wszystkie_znaki, 1)[0]
-
-        haslo = ''.join(random.sample(wynik, len(wynik)))
-        haslo_wynikowe = 'Wygenerowano hasło o długości ' + str(dlugosc_hasla) + ' znaków - ' + str(haslo)
-        return render_template("haslo.html", haslo=haslo_wynikowe)
-    else:
-        return render_template("haslo.html", haslo='Popraw wymagania hasła')
+    haslo = haslo()
+    return render_template("haslo.html", haslo=haslo)
 
 @app.route('/flaga_dla_ukrainy')
 def flaga_dla_ukrainy():
@@ -65,7 +34,6 @@ def flaga_dla_ukrainy():
 def FlagaDlaUkrainy():
     return render_template("flaga-dla-ukrainy.html")
 
-# @app.route('/brudnopis')
 @app.route('/brudnopis')
 def brudnopis():
     super_heroes = ['Bruce Lee', 'Kubuś Puchatek', 'Kopernik', 'Małysz']
---
+++ moje_programy/haslo.py
@@ -0,0 +1,38 @@
+import string
+import random
+
+def haslo():    
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    digits = string.digits
+    punctuation = string.punctuation
+    wszystkie_znaki = lowercase + uppercase + digits + punctuation
+    dlugosc_hasla = 10
+    min_malych_liter = 2
+    min_duzych_liter = 2
+    min_cyfr = 2
+    min_znakow_specjalnych = 2
+    wynik = ''
+    if ((min_cyfr + min_duzych_liter + min_malych_liter + min_znakow_specjalnych) <= dlugosc_hasla):
+        if min_malych_liter > 0:
+            for c in range(min_malych_liter):
+                wynik += random.sample(lowercase, 1)[0]
+        if min_duzych_liter > 0:
+            for c in range(min_duzych_liter):
+                wynik += random.sample(uppercase, 1)[0]
+        if  min_cyfr > 0:
+            for c in range(min_cyfr):
+                wynik += random.sample(digits, 1)[0]
+        if min_znakow_specjalnych > 0:
+            for c in range(min_znakow_specjalnych):
+                wynik += random.sample(punctuation, 1)[0]
+
+        if len(wynik) < (dlugosc_hasla):
+            for c in range(dlugosc_hasla - len(wynik)):
+                wynik += random.sample(wszystkie_znaki, 1)[0]
+
+        haslo = ''.join(random.sample(wynik, len(wynik)))
+        haslo_wynikowe = 'Wygenerowano hasło o długości ' + str(dlugosc_hasla) + ' znaków - ' + str(haslo)
+        return haslo_wynikowe
+    else:
+        return 'Popraw wymagania hasła'
